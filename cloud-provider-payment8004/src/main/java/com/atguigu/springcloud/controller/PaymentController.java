31feb81b3c311bf7a9883cad1587a9c870bfb83a
Djx980831 <1530916212@qq.com>
2021-04-10 18:57:21 +0800

    zookeeper

---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,28 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @param
+ * @Description TODO
+ * @Author dongjingxiong
+ * @return
+ * @Date 2021-04-10 11:52
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/zk")
+    public String paymentZk() {
+        return "springcloud with zookeeper: " + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+}
---
+++ com-consumer-zk-order80/src/main/java/com/atguigui/springcloud/OrderZKMain80.java
@@ -0,0 +1,20 @@
+package com.atguigui.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @param
+ * @Description TODO
+ * @Author dongjingxiong
+ * @return
+ * @Date 2021-04-10 12:32
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderZKMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZKMain80.class, args);
+    }
+}
