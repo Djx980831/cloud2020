6c9788e7852d4f3ba068c75c40da805abeb4429a
Djx980831 <1530916212@qq.com>
2021-04-24 14:22:26 +0800

    gateway

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,15 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @param
+ * @Description TODO
+ * @Author dongjingxiong
+ * @return
+ * @Date 2021-04-24 13:36
+ */
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime time = ZonedDateTime.now();
+        System.out.println(time);
+    }
+}
