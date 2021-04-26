d13b85d06f91fa5aae575764ebf4626c2334641f
Djx980831 <1530916212@qq.com>
2021-04-26 22:46:46 +0800

    config bus

---
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @param
+ * @Description TODO
+ * @Author dongjingxiong
+ * @return
+ * @Date 2021-04-26 21:16
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
