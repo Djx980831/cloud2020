889f45c13d3c9ba6dd2c2110c3e3f08d47fe0c17
Djx980831 <1530916212@qq.com>
2021-04-28 23:49:21 +0800

    springcloud-stream

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/controller/SendMessageController.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.service.IMessageProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @param
+ * @Description TODO
+ * @Author dongjingxiong
+ * @return
+ * @Date 2021-04-28 23:25
+ */
+@RestController
+public class SendMessageController {
+
+    @Resource
+    private IMessageProvider messageProvider;
+
+    @GetMapping("/sendMessage")
+    public String sendMessage() {
+        return messageProvider.send();
+    }
+}
