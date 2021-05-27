b36e110384432890b0db0de3a349cdc17881b814
Djx980831 <1530916212@qq.com>
2021-05-27 23:08:46 +0800

    整合sentinel

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -0,0 +1,25 @@
+package com.atguigu.springcloud.alibaba.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @param
+ * @Description TODO
+ * @Author dongjingxiong
+ * @return
+ * @Date 2021-05-24 23:29
+ */
+@RestController
+public class FlowLimitController {
+
+    @GetMapping("/testA")
+    public String testA() {
+        return "----testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+        return "----testB";
+    }
+}
