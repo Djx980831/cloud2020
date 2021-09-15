e015fbe8033f095540471f8976bb19123f2dd789
tom_glb <goodMorning_glb.@gugui.com>
2021-09-15 23:41:46 +0800

    sentinel 配置

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.atguigu.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -39,5 +42,17 @@ public class FlowLimitController {
 
         int a = 10 / 0;
         return "-------testD";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "deal_testHoyKey")
+    public String testHotkey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false)String p2) {
+        int age = 10 / 0;
+        return "-----testHotKey!!";
+    }
+
+    public String deal_testHoyKey(String p1, String p2, BlockException ex ) {
+        return "-----deal_testHoyKey!!";
     }
 }
