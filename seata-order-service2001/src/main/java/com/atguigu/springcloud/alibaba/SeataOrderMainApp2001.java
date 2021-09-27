3f04abce9b0cef608575d13634adecd6ec02a515
Djx980831 <1530916212@qq.com>
2021-09-28 00:53:11 +0800

    seata-order-service

---
+++ seata-order-service2001/src/main/java/com/atguigu/springcloud/alibaba/SeataOrderMainApp2001.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud.alibaba;
+
+
+import com.atguigu.springcloud.alibaba.config.DataSourceProxyConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication(exclude = DataSourceProxyConfig.class)  //取消数据源的自动创建
+public class SeataOrderMainApp2001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeataOrderMainApp2001.class, args);
+    }
+}
