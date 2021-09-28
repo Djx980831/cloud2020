f076e2d206c3d4122e29341245b15f5510fa1f81
Djx980831 <1530916212@qq.com>
2021-09-29 00:14:45 +0800

    seata-stirage-service2002

---
+++ seata-storage-service2002/src/main/java/com/atguigu/springcloud/alibaba/service/impl/StorageServiceImpl.java
@@ -0,0 +1,23 @@
+package com.atguigu.springcloud.alibaba.service.impl;
+
+import com.atguigu.springcloud.alibaba.dao.StorageDao;
+import com.atguigu.springcloud.alibaba.service.StorageService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+@Slf4j
+public class StorageServiceImpl implements StorageService {
+
+    @Resource
+    private StorageDao storageDao;
+
+    @Override
+    public void decrease(Long productId, Integer count) {
+        log.info("开始做库存更新");
+        storageDao.decrease(productId, count);
+        log.info("库存更新结束");
+    }
+}
