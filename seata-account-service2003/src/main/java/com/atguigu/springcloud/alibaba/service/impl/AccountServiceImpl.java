c22d71b3f255b52eded66b5d22c78f48a2093f1f
Djx980831 <1530916212@qq.com>
2021-09-29 01:25:23 +0800

    test seata全局事务

--- seata-account-service2003/src/main/java/com/atguigu/springcloud/alibaba/service/impl/AccountServiceImpl.java
+++ seata-account-service2003/src/main/java/com/atguigu/springcloud/alibaba/service/impl/AccountServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
@@ -18,6 +19,14 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public void decrease(Long userId, BigDecimal money) {
         log.info("开始更新账户余额");
+
+        //模拟超时异常
+        //停顿20s
+        try {
+            TimeUnit.SECONDS.sleep(20);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         accountDao.decrease(userId, money);
         log.info("更新账户余额完成");
     }
