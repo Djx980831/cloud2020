dbb9dcc80343956c4de516fb2905953957a8b759
Djx980831 <1530916212@qq.com>
2021-09-29 00:41:55 +0800

    seata-account-service2003

---
+++ seata-account-service2003/src/main/java/com/atguigu/springcloud/alibaba/service/impl/AccountServiceImpl.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud.alibaba.service.impl;
+
+import com.atguigu.springcloud.alibaba.dao.AccountDao;
+import com.atguigu.springcloud.alibaba.service.AccountService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.math.BigDecimal;
+
+@Service
+@Slf4j
+public class AccountServiceImpl implements AccountService {
+
+    @Resource
+    private AccountDao accountDao;
+
+    @Override
+    public void decrease(Long userId, BigDecimal money) {
+        log.info("开始更新账户余额");
+        accountDao.decrease(userId, money);
+        log.info("更新账户余额完成");
+    }
+}
